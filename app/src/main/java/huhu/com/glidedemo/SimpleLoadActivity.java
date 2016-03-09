91b3d5a358168726cbc9b94b3a89d583c104f4d6
zhangwen <646689983@qq.com>
2016-03-09 16:12:47 +0800

    初步使用Glide

---
+++ app/src/main/java/huhu/com/glidedemo/SimpleLoadActivity.java
@@ -0,0 +1,26 @@
+package huhu.com.glidedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * 简单使用Glide库加载图片测试
+ */
+public class SimpleLoadActivity extends Activity {
+    //测试图片的ImageView
+    private ImageView mImageView = null;
+    //测试图片的URL
+    private String url = "http://7xl07p.com1.z0.glb.clouddn.com/%60(TZ~5RGO%60G0YVIY281%25ZMB.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.mImageView);
+        //流接口形式
+        Glide.with(SimpleLoadActivity.this).load(url).into(mImageView);
+    }
+}
